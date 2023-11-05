d38d7aed86b6fee5bf80594695a4070cbe866f1e
KerenS-uts <145767087+KerenS-uts@users.noreply.github.com>
2023-11-05 18:26:17 +1100

    Add files via upload

---
+++ Assignment2.m
@@ -0,0 +1,20 @@
+function Assignment2 ()
+axis([-2,2,-2,2,0,4])
+
+hold on 
+PlaceObject('diamond2.ply',[ -2.2, 0 ,0 ]);
+PlaceObject('RoboArm.ply',[ -1, 0 ,0 ])
+PlaceObject('meerkat.ply',[ 0, 0 ,0 ]);
+%PlaceObject('scatteredrock.ply',[ 1, 0 ,0 ]);
+
+PlaceObject('tripod3.ply',[ 1, 0 ,0 ]);
+
+
+%PlaceObject('rockpiles.ply',[ 1, 0 ,0 ]);
+PlaceObject('onerock.ply',[ -1, 0 ,0 ]);
+PlaceObject('buggy1.ply',[ 0, 1 ,0 ]);
+%PlaceObject('enclosure4.ply',[ -1, 1 ,0 ]);
+PlaceObject('enclosure6.ply',[ -1, 1 ,0 ]);
+
+hold on
+
